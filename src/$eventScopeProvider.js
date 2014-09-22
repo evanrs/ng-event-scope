128499595c0cc5704a9dc0c6e9162dee15449d72
Evan Schneider <evanrs@gmail.com>
2014-09-22 12:37:25 -0400

    Removes $rootScope singleton from $rootScopeDecorator.

--- src/$eventScopeProvider.js
+++ src/$eventScopeProvider.js
@@ -2,12 +2,14 @@ var decorate = require('./event-decorator');
 
 module.exports = function ($rootScopeProvider) {
     var $get = $rootScopeProvider.$get.slice(-1)[0];
+
     this.$get = $rootScopeProvider.$get.slice(0, -1).concat([
-        function ($injector, $exceptionHandler, $parse, $browser) {
-        var $rootEventScope = $get.apply($rootScopeProvider, arguments);
+        function () {
+            var $rootEventScope = $get.apply($rootScopeProvider, arguments);
 
-        decorate($rootEventScope.constructor.prototype);
+            decorate($rootEventScope.constructor.prototype);
 
-        return $rootEventScope;
-    }]);
+            return $rootEventScope;
+        }
+    ]);
 }
--- src/$rootScopeDecorator.js
+++ src/$rootScopeDecorator.js
@@ -1,14 +1,16 @@
 var decorate = require('./event-decorator');
-var $rootScope;
 
 module.exports = function ($rootScopeProvider) {
     var $get = $rootScopeProvider.$get.slice(-1)[0];
-    $rootScopeProvider.$get = this.$get =
-        $rootScopeProvider.$get.slice(0, -1).concat([function() {
-        $rootScope = $rootScope || $get.apply($rootScopeProvider, arguments);
 
-        decorate($rootScope.constructor.prototype);
+    $rootScopeProvider.$get =
+    this.$get = $rootScopeProvider.$get.slice(0, -1).concat([
+        function() {
+            var $rootScope = $get.apply($rootScopeProvider, arguments);
 
-        return $rootScope
-    }]);
+            decorate($rootScope.constructor.prototype);
+
+            return $rootScope
+        }
+    ]);
 }
